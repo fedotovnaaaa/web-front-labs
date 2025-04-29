130f6705ae1a240f9004ea2306f5b8a34149c0c2
fedotovnaaaa <julia.fedotoff.2005@gmail.com>
2025-04-30 00:35:21 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,106 @@
+const books = [
+    {
+        author: 'Лия Арден',
+        name: 'Мара и Морок',
+        publicationYear: 2021,
+        price: 760,
+        bestseller: true,
+        inStock: false
+    },
+    {
+        author: 'Нора Сакавич',
+        name: 'Лисья нора',
+        publicationYear: 2020,
+        price: 897,
+        bestseller: false,
+        inStock: true
+    },    
+    {
+        author: 'Харуки Мураками',
+        name: 'Норвежский лес',
+        publicationYear: 1987,
+        price: 934,
+        bestseller: true,
+        inStock: true
+    },
+    {
+        author: 'Шестерка воронов',
+        name: 'Ли Бардуго',
+        publicationYear: 2015,
+        price: 1098,
+        bestseller: false,
+        inStock: false
+    },
+    {
+        author: 'Игра престолов',
+        name: 'Джордж Мартин',
+        publicationYear: 1996,
+        price: 1534,
+        bestseller: true,
+        inStock: true
+    },    
+    {
+        author: 'Агата Кристи',
+        name: 'Убийства по алфавиту',
+        publicationYear: 1987,
+        price: 358,
+        bestseller: false,
+        inStock: true
+    },    
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 314,
+        bestseller: true,
+        inStock: false
+    },    
+    {
+        author: 'Михаил Булгаков',
+        name: 'Мастер и Маргарита',
+        publicationYear: 1966,
+        price: 284,
+        bestseller: false,
+        inStock: true
+    },    
+    {
+        author: 'Хадзимэ Исаяма',
+        name: 'Атака титанов. Книга 1 (Том 1, 2) ',
+        publicationYear: 2021,
+        price: 1103,
+        bestseller: true,
+        inStock: false
+    },    
+    {
+        author: 'Дэниел Киз',
+        name: 'Цветы для Элджернона',
+        publicationYear: 1966,
+        price: 551,
+        bestseller: true,
+        inStock: true
+    },    
+    {
+        author: 'Джонатан Фоер',
+        name: 'Жутко громко и запредельно близко',
+        publicationYear: 2005,
+        price: 901,
+        bestseller: false,
+        inStock: false
+    },    
+    {
+        author: 'Рэй Брэдбери',
+        name: 'Вино из одуванчиков',
+        publicationYear: 1957,
+        price: 551,
+        bestseller: false,
+        inStock: false
+    },    
+    {
+        author: 'Оскар Уайльд',
+        name: 'Портрет Дориана Грея',
+        publicationYear: 1890,
+        price: 459,
+        bestseller: false,
+        inStock: true
+    },
+];
